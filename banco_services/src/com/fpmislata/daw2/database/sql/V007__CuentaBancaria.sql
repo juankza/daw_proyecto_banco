e0d86706c6745c818b75007ff4290244f50b8bf6
juankza <juankza@github.com>
2016-01-13 10:37:56 +0100

    chore - flyway scripts

---
+++ banco_services/src/com/fpmislata/daw2/database/sql/V007__CuentaBancaria.sql
@@ -0,0 +1,17 @@
+
+CREATE TABLE `cuentabancaria` (
+  `idCuentaBancaria` int(11) NOT NULL,
+  `saldo` decimal(10,2) DEFAULT NULL,
+  `fechaCreacion` datetime DEFAULT NULL,
+  `pin` varchar(4) DEFAULT NULL,
+  `digitoControl` varchar(2) DEFAULT NULL,
+  `numeroCuenta` varchar(10) DEFAULT NULL,
+  `idSucursalBancaria` int(11) NOT NULL,
+  `idUsuario` int(11) NOT NULL,
+  PRIMARY KEY (`idCuentaBancaria`),
+  UNIQUE KEY `numeroCuenta` (`numeroCuenta`),
+  INDEX `fk_idSucursalBancaria` (`idSucursalBancaria`),
+  INDEX `fk_idUsuario` (`idUsuario`),
+  CONSTRAINT `fk_idSucursalBancaria` FOREIGN KEY (`idSucursalBancaria`) REFERENCES `sucursalbancaria` (`idSucursalBancaria`),
+  CONSTRAINT `fk_idUsuario` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
---
+++ banco_services/src/com/fpmislata/daw2/database/sql/V008__MovimientoBancario.sql
@@ -0,0 +1,12 @@
+
+CREATE TABLE `movimientobancario` (
+  `idMovimientoBancario` int(11) NOT NULL,
+  `tipo` varchar(9) DEFAULT NULL,
+  `concepto` varchar(255) DEFAULT NULL,
+  `cantidad` decimal(10,2) DEFAULT NULL,
+  `fecha` datetime DEFAULT NULL,
+  `idCuentaBancaria` int(11) NOT NULL,
+  PRIMARY KEY (`idMovimientoBancario`),
+  INDEX `fk_idCuentaBancaria` (`idCuentaBancaria`),
+  CONSTRAINT `fk_idCuentaBancaria` FOREIGN KEY (`idCuentaBancaria`) REFERENCES `cuentabancaria` (`idCuentaBancaria`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
