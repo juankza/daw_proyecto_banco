b47c699c0e786f94a144625cd2289861b76881cd
Lliurex <Lliurex@172.16.205.123>
2016-01-19 14:16:52 +0100

    feat: maestro/detalle 100% bien hecho a nivel angular

--- banco/web/cuentabancaria/cuentabancaria-delete.js
+++ banco/web/cuentabancaria/cuentabancaria-delete.js
@@ -4,6 +4,9 @@ function CuentaBancariaDeleteController(cuentaBancariaService, $scope, $routePar
         $scope.cuentaBancaria = data;
         var fechaCreacion = new Date(data.fechaCreacion);
         $scope.cuentaBancaria.fechaCreacion = fechaCreacion.getFullYear() + "-" + (fechaCreacion.getMonth() + 1) + "-" + fechaCreacion.getDate();
+        $scope.ccc = $scope.cuentaBancaria.sucursalBancaria.entidadBancaria.codigoEntidadBancaria + " " + $scope.cuentaBancaria.sucursalBancaria.codigoSucursalBancaria + " " + $scope.cuentaBancaria.digitoControl + " " + $scope.cuentaBancaria.numeroCuenta;
+        $scope.sucursal = $scope.cuentaBancaria.sucursalBancaria.codigoSucursalBancaria + "  " + $scope.cuentaBancaria.sucursalBancaria.direccion;
+        $scope.usuario = $scope.cuentaBancaria.usuario.nombre + " " + $scope.cuentaBancaria.usuario.apellidos;
     }).error(function (data, status, headers, config) {
         alert("HTTP request failed. Status: " + status);
     });
