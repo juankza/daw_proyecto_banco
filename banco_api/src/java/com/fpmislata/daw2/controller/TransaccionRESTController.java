c1120ecf4b4228bd6aaed1d98469178ac631f3b2
vicenteasd <vicenteasd@github.com>
2016-01-29 10:09:50 +0100

    feat: transaccion entre dos cuentas funciona

---
+++ banco_api/src/java/com/fpmislata/daw2/controller/TransaccionRESTController.java
@@ -0,0 +1,54 @@
+package com.fpmislata.daw2.controller;
+
+import com.fpmislata.daw2.business.domain.Transaccion;
+import com.fpmislata.daw2.business.service.TransaccionService;
+import com.fpmislata.daw2.core.exception.BusinessException;
+import com.fpmislata.daw2.core.json.JSONTransformer;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/transaccion")
+public class TransaccionRESTController {
+
+    @Autowired
+    JSONTransformer jsonTransformer;
+    @Autowired
+    TransaccionService transaccionService;
+
+    @RequestMapping(method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
+    public void insert(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, @RequestBody String jsonRequest) {
+        Transaccion newTransaccion = null, insertedTransaccion = null;
+
+        try {
+            newTransaccion = jsonTransformer.toObject(jsonRequest, Transaccion.class);
+            insertedTransaccion = transaccionService.insertaTransaccion(newTransaccion);
+
+            if (insertedTransaccion != null) {
+                httpServletResponse.setStatus(HttpServletResponse.SC_OK);
+                httpServletResponse.setContentType("application/json; charset=UTF-8");
+                httpServletResponse.getWriter().println(jsonTransformer.toJSON(insertedTransaccion));
+            }
+        } catch (BusinessException bex) {
+            try {
+                httpServletResponse.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                httpServletResponse.setCharacterEncoding("application/json; charset=UTF-8");
+                httpServletResponse.getWriter().println(jsonTransformer.toJSON(bex.getBusinessMessages()));
+            } catch (IOException ex) {
+                httpServletResponse.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+                Logger.getLogger(CuentaBancariaRESTController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } catch (IOException ex) {
+            httpServletResponse.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            Logger.getLogger(CuentaBancariaRESTController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
