2cb344ad0fbd3fd3cb98111b4e40f5973aa3c1c3
vicenteasd <vicenteasd@github.com>
2016-02-01 10:51:20 +0100

    feat: comprobar los datos de transaccion (por acabar)

--- banco_services/src/com/fpmislata/daw2/business/service/impl/TransaccionServiceImpl.java
+++ banco_services/src/com/fpmislata/daw2/business/service/impl/TransaccionServiceImpl.java
@@ -29,11 +29,45 @@ public class TransaccionServiceImpl extends GenericServiceImpl<Transaccion, Inte
     @Override
     public Transaccion insertaTransaccion(Transaccion transaccion) throws BusinessException {
         MovimientoBancario movimientoBancarioOrigen = null, movimientoBancarioDestino = null;
+        
+        if(transaccion.getCuentaOrigen().length() != 20) {
+            throw new BusinessException(new BusinessMessage("Cuenta Origen","Introduce los datos de la cuenta origen correctamente"));
+        }
+        if(transaccion.getCuentaDestino().length() != 20) {
+            throw new BusinessException(new BusinessMessage("Cuenta Destino","Introduce los datos de la cuenta origen correctamente"));
+        }
+        String numEntidadBancariaOrigen = transaccion.getCuentaOrigen().trim().substring(0, 4);
+        String numSucursalBancariaOrigen = transaccion.getCuentaOrigen().trim().substring(4, 8);
+        String numDigitoControlOrigen = transaccion.getCuentaOrigen().trim().substring(8, 10);
         String numCuentaBancariaOrigen = transaccion.getCuentaOrigen().trim().substring(10, 20);
+        
+        String numEntidadBancariaDestino = transaccion.getCuentaOrigen().trim().substring(0, 4);
+        String numSucursalBancariaDestino = transaccion.getCuentaOrigen().trim().substring(4, 8);
+        String numDigitoControlDestino = transaccion.getCuentaOrigen().trim().substring(8, 10);
         String numCuentaBancariaDestino = transaccion.getCuentaDestino().trim().substring(10, 20);
 
+        // Se puede optimizar guardando en un objeto para que no haya que hacer la petición dos veces
+        if(cuentaBancariaDAO.findByNumeroCuenta(numCuentaBancariaOrigen).size() == 0) {
+           throw new BusinessException(new BusinessMessage("Cuenta Origen","Introduce los datos de la cuenta orígen corectamente."));
+        }
+        if(cuentaBancariaDAO.findByNumeroCuenta(numCuentaBancariaDestino).size() == 0) {
+           throw new BusinessException(new BusinessMessage("Cuenta Destino","Introduce los datos de la cuenta orígen corectamente."));
+        }
         CuentaBancaria cuentaBancariaOrigen = cuentaBancariaDAO.findByNumeroCuenta(numCuentaBancariaOrigen).get(0);
         CuentaBancaria cuentaBancariaDestino = cuentaBancariaDAO.findByNumeroCuenta(numCuentaBancariaDestino).get(0);
+        // Comprobar que los cuatro primeros digitos (cod entidad coinciden con su entidad;
+        // que los cuatro segundos con la sucursal y los dos siguientes con el dígito de control.
+        // ---- el código de error está bien?
+        if(cuentaBancariaOrigen.getSucursalBancaria().getEntidadBancaria().getCodigoEntidadBancaria() != numEntidadBancariaOrigen) {
+            throw new BusinessException(new BusinessMessage("Cuenta Origen","Introduce el código de entidad correctamente"));
+        }
+        if(cuentaBancariaOrigen.getSucursalBancaria().getEntidadBancaria().getCodigoEntidadBancaria() != numSucursalBancariaOrigen) {
+            throw new BusinessException(new BusinessMessage("Cuenta Origen","Introduce el código de sucursal correctamente"));
+        }
+        if(cuentaBancariaOrigen.getDigitoControl() != numDigitoControlOrigen) {
+            throw new BusinessException(new BusinessMessage("Cuenta Origen","Introduce el dígito de control correctamente"));
+        }
+        
         //si el importe es negativo esto; si es positivo al contrario
         if (transaccion.getImporte().compareTo(BigDecimal.ZERO) > 0) {
             movimientoBancarioOrigen = new MovimientoBancario(TipoMovimientoBancario.INGRESO, transaccion.getConcepto(), transaccion.getImporte(), cuentaBancariaOrigen.getSaldo().add(transaccion.getImporte()), new Date(), cuentaBancariaOrigen);
