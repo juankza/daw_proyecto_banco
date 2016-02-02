763d5f5f13139a6e56f7f97b969f6aae88f7d9eb
vicenteasd <vicenteasd@github.com>
2016-02-02 14:46:40 +0100

    fix: columnas list ordenadas
    Añadidas las columnas con datos relevantes y quitadas las de ID

--- banco/web/movimientobancario/movimientobancario-detail.js
+++ banco/web/movimientobancario/movimientobancario-detail.js
@@ -3,6 +3,8 @@ function MovimientoBancarioDetailController($scope,movimientoBancarioService,$ro
     var response = movimientoBancarioService.detail($routeParams.idMovimientoBancario);
     response.success(function (data, status, headers, config) {
         $scope.movimientoBancario = data;
+        $scope.movimientoBancario.cantidad += "€";
+        $scope.movimientoBancario.saldo += "€";
         $scope.movimientoBancario.fecha = new Date($scope.movimientoBancario.fecha);
         $scope.movimientoBancario.fecha = $scope.movimientoBancario.fecha.getFullYear() + "-" + ($scope.movimientoBancario.fecha.getMonth()+1) + "-" + $scope.movimientoBancario.fecha.getDate();
     }).error(function (data, status, headers, config) {
