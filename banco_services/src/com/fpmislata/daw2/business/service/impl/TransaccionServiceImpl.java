761e93292f12a1950c9371ef5efcdf2430f4be6f
vicenteasd <vicenteasd@github.com>
2016-02-04 12:31:25 +0100

    fix: por arrreglar comprobacion

--- banco_services/src/com/fpmislata/daw2/business/service/impl/TransaccionServiceImpl.java
+++ banco_services/src/com/fpmislata/daw2/business/service/impl/TransaccionServiceImpl.java
@@ -10,6 +10,7 @@ import com.fpmislata.daw2.business.service.TransaccionService;
 import com.fpmislata.daw2.core.exception.BusinessException;
 import com.fpmislata.daw2.core.exception.BusinessMessage;
 import com.fpmislata.daw2.persistence.dao.CuentaBancariaDAO;
+import com.fpmislata.daw2.persistence.dao.EntidadBancariaDAO;
 
 import java.math.BigDecimal;
 
@@ -18,6 +19,8 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class TransaccionServiceImpl extends GenericServiceImpl<Transaccion, Integer> implements TransaccionService {
+    @Autowired
+    EntidadBancariaDAO entidadBancariaDAO;
     @Autowired
     CuentaBancariaDAO cuentaBancariaDAO;
     @Autowired
@@ -41,8 +44,8 @@ public class TransaccionServiceImpl extends GenericServiceImpl<Transaccion, Inte
         String numEntidadBancariaDestino = transaccion.getCuentaOrigen().trim().substring(0, 4);
         String numSucursalBancariaDestino = transaccion.getCuentaOrigen().trim().substring(4, 8);
         String numDigitoControlDestino = transaccion.getCuentaOrigen().trim().substring(8, 10);
-        String numCuentaBancariaDestino = transaccion.getCuentaDestino().trim().substring(10, 20);
-
+        String numCuentaBancariaDestino = transaccion.getCuentaDestino().trim().substring(10, 20);     
+                
         // Se puede optimizar guardando en un objeto para que no haya que hacer la petición dos veces
         if(cuentaBancariaDAO.findByNumeroCuenta(numCuentaBancariaOrigen).size() == 0) {
            throw new BusinessException(new BusinessMessage("Cuenta Origen","Introduce los datos de la cuenta orígen corectamente"));
@@ -56,16 +59,20 @@ public class TransaccionServiceImpl extends GenericServiceImpl<Transaccion, Inte
         // que los cuatro segundos con la sucursal y los dos siguientes con el dígito de control.
         // ---- el código de error está bien?
         
-        /*DA ERROR PQ LA ENTIDAD ES NULL
+        //DA ERROR PQ LA ENTIDAD ES NULL
         if(cuentaBancariaOrigen.getSucursalBancaria().getEntidadBancaria().getCodigoEntidadBancaria() != numEntidadBancariaOrigen) {
             throw new BusinessException(new BusinessMessage("Cuenta Origen","Introduce el código de entidad correctamente"));
         }
         if(cuentaBancariaOrigen.getSucursalBancaria().getCodigoSucursalBancaria() != numSucursalBancariaOrigen) {
             throw new BusinessException(new BusinessMessage("Cuenta Origen","Introduce el código de sucursal correctamente"));
-        }*/
+        }
         if(!cuentaBancariaOrigen.getDigitoControl().equals(numDigitoControlOrigen)) {
             throw new BusinessException(new BusinessMessage("Cuenta Origen","Introduce el dígito de control correctamente"));
         }
+        
+        
+        
+        
         /*BigDecimal importe;
         try {
             importe = transaccion.getImporte();
