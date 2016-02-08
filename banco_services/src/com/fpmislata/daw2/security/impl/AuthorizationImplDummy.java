f2d02979deed38a0c2454a27327f1780f09c2049
Lliurex <Lliurex@172.16.205.123>
2016-02-08 13:30:19 +0100

    feat: añadiendo parametros a build.xml, activada autorizacion en url de transaccion

--- banco_services/src/com/fpmislata/daw2/security/impl/AuthorizationImplDummy.java
+++ banco_services/src/com/fpmislata/daw2/security/impl/AuthorizationImplDummy.java
@@ -10,10 +10,11 @@ public class AuthorizationImplDummy implements Authorization {
 
     private final String URL_API = "/api";
     private final String URL_SESSION = URL_API + "/session";
+    private final String URL_TRANSACCION = URL_API + "/transaccion";
     
     @Override
     public boolean isAuthorizedURL(Usuario usuario, String url, HTTPMethod httpmethod) {
-        if(url.equals(URL_API) || url.equals(URL_API + "/") || url.equals(URL_SESSION) || url.equals(URL_SESSION + "/")) {
+        if(url.equals(URL_API) || url.equals(URL_API + "/") || url.equals(URL_SESSION) || url.equals(URL_SESSION + "/") || url.equals(URL_TRANSACCION)) {
             return true;
         } else {
             if(usuario != null) {
